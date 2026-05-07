32bfe62e50e8125fd70f65799e26e3bcc66262eb
KeerthanaS-004 <keerthana.s0404@gmail.com>
2026-05-07 10:42:39 +0530

    Array of objects

---
+++ src/oops/arrayOfObject.java
@@ -0,0 +1,31 @@
+package oops;
+
+class students{
+    int marks;
+    String name;
+}
+
+public class arrayOfObject {
+    static void main(String[] args) {
+        students s1 =  new students();
+        s1.name = "conrad";
+        s1.marks = 86;
+
+        students s2 =  new students();
+        s2.name = "jeremey";
+        s2.marks = 90;
+
+        students s3 =  new students();
+        s3.name = "lily";
+        s3.marks = 80;
+
+        students arr[] = new students[3];
+        arr[0] = s1;
+        arr[1] = s2;
+        arr[2] = s3;
+        for (int i = 0; i < arr.length ; i++) {
+            System.out.println(arr[i].name + " : " + arr[i].marks);
+        }
+    }
+
+}
