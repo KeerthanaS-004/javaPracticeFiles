54c2f08c92cac881ebeacec2116700d56a94efda
KeerthanaS-004 <keerthana.s0404@gmail.com>
2026-05-26 17:16:52 +0530

    encapsulation

---
+++ src/oops/privateVariables.java
@@ -0,0 +1,32 @@
+package oops;
+class human{
+    String name;
+    int age;
+    String email;
+    private int password;
+    //create a method to access this private var in other classes
+    public int getPassword() {
+        return password;
+    }
+    //to assign value create a method that accepts the instance variable as args
+    public void setPassword(int pass) {
+        password = pass;
+    }
+
+}
+public class privateVariables {
+    static void main(String[] args) {
+        human obj = new human();
+        obj.name = "apple";
+        obj.age = 20;
+        obj.email = "apple@gmail.com";
+        //to access the private variable , use methods for that
+        System.out.println(obj.name);
+        System.out.println(obj.age);
+        System.out.println(obj.email);
+        obj.setPassword(12);
+        System.out.println(obj.getPassword());
+
+
+    }
+}
