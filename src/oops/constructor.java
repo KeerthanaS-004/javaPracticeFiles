4858def639454040b8114ea5533fd93512962cce
KeerthanaS-004 <keerthana.s0404@gmail.com>
2026-05-30 11:52:09 +0530

    constructor

--- src/oops/constructor.java
+++ src/oops/constructor.java
@@ -1,5 +1,21 @@
 package oops;
-
-public class constructor
-    {
+class college{
+    String name;
+    String city;
+    public college(){
+        name = "Hindusthan";
+        city = "Coimbatore";
+    }
+    public college(String name, String city){ //parametarized constructor
+        this.name = name;
+        this.city = city;
+    }
+}
+public class constructor{
+    static void main(String[] args) {
+        college ob1 = new college();
+        college ob2 = new college("karpagam", "CBE");
+        System.out.println(ob1.name + ":" + ob1.city);
+        System.out.println(ob2.name +  ":" + ob2.city);
+    }
 }
