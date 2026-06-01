40166ff0a0929d49298fa46cf09caaec316d52d3
KeerthanaS-004 <keerthana.s0404@gmail.com>
2026-06-01 21:02:14 +0530

    multi level inher

---
+++ src/oops/inheritance/advCal1.java
@@ -0,0 +1,7 @@
+package oops.inheritance;
+
+public class advCal1 extends advCal{
+    public double power(int a, int b){
+        return Math.pow(a,b);
+    }
+}
--- src/oops/inheritance/main.java
+++ src/oops/inheritance/main.java
@@ -2,11 +2,12 @@ package oops.inheritance;
 
 public class main {
     static void main(String[] args) {
-        advCal c = new advCal();
+        advCal1 c = new advCal1();
         int n1 = c.add(4,4);
         int n2 = c.sub(6,2);
         int n3 = c.mul(2,3);
         int n4 = c.div(9,3);
-        System.out.println(n1 + " " + n2 + " " + n3 + " " + n4);
+        double n5 = c.power(2, 3);
+        System.out.println(n1 + " " + n2 + " " + n3 + " " + n4 + " " + n5);
     }
 }
